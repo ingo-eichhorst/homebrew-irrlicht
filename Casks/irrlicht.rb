db531fa037f3f10035c7f3e47be044e72e4a2959
Ingo Eichhorst <info@ingo-eichhorst.de>
2026-08-24 00:22:44 +0200

    irrlicht 0.6.0

--- Casks/irrlicht.rb
+++ Casks/irrlicht.rb
@@ -1,6 +1,6 @@
 cask "irrlicht" do
-  version "0.5.10"
-  sha256 "04f201018355594eedb9d1d334098afa4677ce87e512a03b3db6a17f3bf80d95"
+  version "0.6.0"
+  sha256 "b6a317f36c0fd3b97d0a999a8a0d588f5cf852d4c72a3e98ed68d41b7a3dd55c"
 
   url "https://github.com/ingo-eichhorst/Irrlicht/releases/download/v#{version}/Irrlicht-#{version}.dmg",
       verified: "github.com/ingo-eichhorst/Irrlicht/"
